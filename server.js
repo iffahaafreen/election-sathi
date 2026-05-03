ac9307956bce630f81941ac732207cc32daac1c9
Iffah Aafreen <159646529+iffahaafreen@users.noreply.github.com>
2026-05-03 22:18:21 +0530

    Implement basic HTTP server in server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const mimeTypes = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.svg': 'image/svg+xml',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon'
+};
+
+http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = '.' + filePath;
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      fs.readFile('./index.html', (err2, data2) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data2);
+      });
+      return;
+    }
+    const ext = path.extname(filePath);
+    res.writeHead(200, { 'Content-Type': mimeTypes[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log('Server running on port ' + PORT));
